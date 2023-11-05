57b89e7db7d23b44aa7bfd5a3cda794049ecaa1b
mdchad <irsyad.muhd@gmail.com>
2023-11-05 16:53:29 +0800

    upgrade

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
 import { defineConfig } from "vite";
 import { resolve } from 'path'
 import react from "@vitejs/plugin-react";
-import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
-  },
-  rollupOptions: {
-    input: {
-      landing: resolve(__dirname, 'index.html'),
-      app: resolve(__dirname, 'src/app/index.html')
-    }
   }
 });
